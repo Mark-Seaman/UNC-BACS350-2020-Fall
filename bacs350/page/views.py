c9cc95d4bf80d715cc21c62ea3a746b8cd702bef
Mark Seaman <Mark.Seaman@imac.net>
2020-07-15 11:08:41 -0600

    Create Documents folder

--- bacs350/page/views.py
+++ bacs350/page/views.py
@@ -3,9 +3,15 @@ from django.views.generic import TemplateView
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, world.</h1> <p>This is a simple HTML page.</p>")
+    html = "<h1>Hello, world.</h1>" 
+    html += ""<p>This is a simple HTML page.</p>"
+    html += ""<p>This is a simple HTML page.</p>"
+    return HttpResponse(html)
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
     
+
+class IndexPageView(TemplateView):
+    template_name = 'index.html'
