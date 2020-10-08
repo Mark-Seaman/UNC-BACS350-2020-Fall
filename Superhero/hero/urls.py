46b2d0e87edd24734f5a7a1bc8694f0795961f76
Mark Seaman <Mark.Seaman@imac.net>
2020-10-08 09:42:41 -0600

    Superhero Demo and remove pycache files

--- Superhero/hero/urls.py
+++ Superhero/hero/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', HeroView.as_view()),
-    path('about', AboutPage.as_view()),
-    path('base', BasePage.as_view()),
     path('<str:identity>', HeroView.as_view()),
 ]
 
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,15 +1,24 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from .models import Superhero
 
 
 class HeroView(TemplateView):
     template_name = "hero.html"
 
     def get_context_data(self, **kwargs):
-        id = kwargs.get('identity', 'Hulk')
-        return {'hero': id, 'css': '/static/hero.css'}
+        heroes = Superhero.objects.all()
+#        heroes = ['hulk', 'iron_man', 'black_widow']
+        return {'heroes': heroes, 'css': '/static/hero.css'}
+ 
+class AddHeroView(CreateView):
+    template_name = "hero_add.html"
+    model = Superhero
+    
 
 class BasePage(TemplateView):
     template_name = "page_theme.html"
+    
     
 class AboutPage(TemplateView):
     template_name = "about.html"
