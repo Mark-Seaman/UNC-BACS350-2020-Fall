5710b93c3e14e0b0b6d13ef4f172efb86689a5df
Mark Seaman <Mark.Seaman@imac.net>
2020-10-12 15:22:35 -0600

    Superhero views with Crispy forms

--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -32,3 +32,7 @@ class HeroEditView(UpdateView):
     model = Superhero
     fields = '__all__'
     
+    def get_context_data(self, **kwargs):
+        kwargs = super(HeroEditView, self).get_context_data(**kwargs)
+        kwargs['edit'] = True
+        return kwargs
