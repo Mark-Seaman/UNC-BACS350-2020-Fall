3393fafec5ede101e9022b4b12c2ea9183f81150
Mark Seaman <Mark.Seaman@imac.net>
2020-10-16 15:20:16 -0600

    deploy Lesson 23

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class Superhero(models.Model):
     name = models.CharField(max_length=20)
     identity = models.CharField(max_length=20)
-    image = models.CharField(max_length=20, null=True)
+    image = models.CharField(max_length=200, null=True)
     
     def __str__(self):
         return self.identity
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView, ListView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView
+from os.path import exists
+
 from .models import Superhero
 
 
@@ -15,7 +17,15 @@ class HeroDetailView(DetailView):
     template_name = "hero_detail.html"
     model = Superhero
     
-    
+    def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+        image = kwargs['object'].image
+        if not exists('static/' + image):
+            kwargs['missing'] = True
+        return kwargs
+            
+   
+
 class HeroListView(ListView):
     template_name = "hero_list.html"
     model = Superhero
@@ -28,11 +38,7 @@ class HeroAddView(CreateView):
     
     
 class HeroEditView(UpdateView):
-    template_name = "hero_edit.html"
+    template_name = "hero_add.html"
     model = Superhero
     fields = '__all__'
     
-    def get_context_data(self, **kwargs):
-        kwargs = super(HeroEditView, self).get_context_data(**kwargs)
-        kwargs['edit'] = True
-        return kwargs
