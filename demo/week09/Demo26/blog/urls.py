b98d389075fc2e504988e02aef3e518ea6fe8e70
Mark Seaman <Mark.Seaman@imac.net>
2020-10-23 17:15:18 -0600

    deploy Lesson 26 and lecture post

--- demo/week09/Demo26/blog/urls.py
+++ demo/week09/Demo26/blog/urls.py
@@ -9,9 +9,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path('post/<int:pk>/delete/',   BlogDeleteView.as_view(),   name='post_delete'),
-    path('post/add',                BlogCreateView.as_view(),   name='post_add'),
-    path('post/<int:pk>/',          BlogDetailView.as_view(),   name='post_detail'),
-    path('post/<int:pk>/edit/',     BlogUpdateView.as_view(),   name='post_edit'),
-    path('',                        BlogListView.as_view(),     name='post_list'),
+    
+    path('post/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/add',             BlogCreateView.as_view(), name='post_add'),
+    path('post/<int:pk>',        BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/',       BlogUpdateView.as_view(), name='post_edit'),
+    path('',                     BlogListView.as_view(),   name='post_list'),
+    
 ]
